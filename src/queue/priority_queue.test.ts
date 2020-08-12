66ebc6a67e3d9b74546d13415efe3322ff3baa43
SuperManEver <nikita.luparev@gmail.com>
2020-08-13 01:54:42 +0500

    minor tweaks

--- src/queue/priority_queue.test.ts
+++ src/queue/priority_queue.test.ts
@@ -3,11 +3,13 @@ import { Patient, PriorityQueue } from './priority_queue'
 test('test priority queue', () => {
   const p1 = new Patient('Andrew', 3)
   const p2 = new Patient('Roman', 1)
+  const p3 = new Patient('Ivan', 5)
 
   const q = new PriorityQueue<Patient>()
 
   q.enqueue(p2)
   q.enqueue(p1)
+  q.enqueue(p3)
 
-  expect(q.dequeue()).toEqual(p1)
+  expect(q.dequeue()).toEqual(p3)
 })
